07b1525bbc355b047775505b821ee95549588477
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-09-17 19:58:56 -0300

    Enum que representa o tipo de veículo.

--- src/veiculo/VeiculoTipo.java
+++ src/veiculo/VeiculoTipo.java
@@ -2,4 +2,17 @@ package veiculo;
 
 public enum VeiculoTipo {
 
+	MOTO("Moto"),
+	CARRO("Carro")
+	;
+	
+	private String descricao;
+
+	VeiculoTipo(String descricao){
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }
